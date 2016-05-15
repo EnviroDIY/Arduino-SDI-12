91750eae7e2ff05ef51bbe8f399787c8003a5a64
Kees Bakker <kees@sodaq.com>
2016-05-15 23:13:22 +0200

    Change data pin in simple logger example to D2

--- examples/d_simple_logger/d_simple_logger.ino
+++ examples/d_simple_logger/d_simple_logger.ino
@@ -63,7 +63,7 @@
 
 #include <SDI12.h>
 
-#define DATAPIN 9         // change to the proper pin
+#define DATAPIN 2         // change to the proper pin
 SDI12 mySDI12(DATAPIN); 
 
 // keeps track of active addresses
