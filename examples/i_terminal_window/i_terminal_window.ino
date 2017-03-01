c1214416c3274093139ddb128810c258952d5a3a
SRGDamia1 <sdamiano@stroudcenter.org>
2017-03-01 17:51:41 -0500

    Renamed terminal example, fixed wait in examples

--- examples/i_terminal_window/i_terminal_window.ino
+++ examples/i_terminal_window/i_terminal_window.ino
@@ -1,5 +1,5 @@
 /*
-Example E: Using the Arduino as a command terminal for SDI-12 sensors.
+Example I: Using the Arduino as a command terminal for SDI-12 sensors.
 Edited by Ruben Kertesz for ISCO Nile 502 2/10/2016…//functions as terminal
 This is a simple demonstration of the SDI-12 library for Arduino.
 It's purpose is to allow a user to interact with an SDI-12 sensor directly,
