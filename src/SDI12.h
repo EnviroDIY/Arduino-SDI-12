b1b5d1c1db010fdb2032634c7f665e4517c6c03a
SRGDamia1 <sdamiano@stroudcenter.org>
2017-05-11 16:05:47 -0400

    Added parity fxn, cleaned up tabs and white spaces

--- src/SDI12.h
+++ src/SDI12.h
@@ -39,57 +39,57 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef SDI12_h
 #define SDI12_h
 
-								//  Import Required Libraries
+    //  Import Required Libraries
 #include <avr/interrupt.h>      // interrupt handling
-#include <util/parity.h>         // optimized parity bit handling
-#include <inttypes.h>			// integer types library
+#include <inttypes.h>           // integer types library
 #include <Arduino.h>            // Arduino core library
-#include <Stream.h>				// Arduino Stream library
+#include <Stream.h>             // Arduino Stream library
 
 typedef const __FlashStringHelper *FlashString;
 
 class SDI12 : public Stream
 {
 protected:
-  int peekNextDigit();			// override of Stream equivalent to allow custom TIMEOUT
+  int peekNextDigit();            // override of Stream equivalent to allow custom TIMEOUT
 private:
-  static SDI12 *_activeObject;	// static pointer to active SDI12 instance
-  void setState(uint8_t state); // sets the state of the SDI12 objects
-  void wakeSensors();			// used to wake up the SDI12 bus
-  void writeChar(uint8_t out); 	// used to send a char out on the data line
-  void receiveChar();			// used by the ISR to grab a char from data line
+  static SDI12 *_activeObject;    // static pointer to active SDI12 instance
+  void setState(uint8_t state);   // sets the state of the SDI12 objects
+  void wakeSensors();             // used to wake up the SDI12 bus
+  void writeChar(uint8_t out);    // used to send a char out on the data line
+  void receiveChar();             // used by the ISR to grab a char from data line
 
   static const char * getStateName(uint8_t state);     // get state name (in ASCII)
 
+  #ifndef __AVR__
+    static uint8_t parity_even_bit(uint8_t v);
+  #endif
+
 public:
   int TIMEOUT;
-  SDI12(uint8_t dataPin);		// constructor
-  ~SDI12();						// destructor
-  void begin();					// enable SDI-12 object
-  void end();					// disable SDI-12 object
+  SDI12(uint8_t dataPin);        // constructor
+  ~SDI12();                      // destructor
+  void begin();                  // enable SDI-12 object
+  void end();                    // disable SDI-12 object
 
-  void forceHold(); 			// sets line state to HOLDING
-  void forceListen(); 			// sets line state to LISTENING
-  void sendCommand(String &cmd);	// sends the String cmd out on the data line
-  void sendCommand(const char *cmd);	// sends the String cmd out on the data line
-  void sendCommand(FlashString cmd);	// sends the String cmd out on the data line
-  void sendResponse(String &resp);	// sends the String resp out on the data line
-  void sendResponse(const char *resp);	// sends the String resp out on the data line
-  void sendResponse(FlashString resp);	// sends the String resp out on the data line
+  void forceHold();                     // sets line state to HOLDING
+  void forceListen();                   // sets line state to LISTENING
+  void sendCommand(String &cmd);        // sends the String cmd out on the data line
+  void sendCommand(const char *cmd);    // sends the String cmd out on the data line
+  void sendCommand(FlashString cmd);    // sends the String cmd out on the data line
+  void sendResponse(String &resp);      // sends the String resp out on the data line (for slave use)
+  void sendResponse(const char *resp);  // sends the String resp out on the data line (for slave use)
+  void sendResponse(FlashString resp);  // sends the String resp out on the data line (for slave use)
 
-  int available();			// returns the number of bytes available in buffer
-  int peek();				// reveals next byte in buffer without consuming
-  int read();				// returns next byte in the buffer (consumes)
-  void flush();				// clears the buffer
-  virtual size_t write(uint8_t byte){return 1;} // dummy function required to inherit from Stream
+  int available();            // returns the number of bytes available in buffer
+  int peek();                 // reveals next byte in buffer without consuming
+  int read();                 // returns next byte in the buffer (consumes)
+  void flush();               // clears the buffer
+  virtual size_t write(uint8_t byte){return 1;}  // dummy function required to inherit from Stream
 
-  bool setActive(); 		// set this instance as the active SDI-12 instance
-  bool isActive();			// check if this instance is active
+  bool setActive();         // set this instance as the active SDI-12 instance
+  bool isActive();          // check if this instance is active
 
-  static inline void handleInterrupt(); // intermediary used by the ISR
-
-  static void setDiagStream(Stream & stream);
-  static void setDiagStream(Stream * stream);
+  static inline void handleInterrupt();  // intermediary used by the ISR
 };
 
 #endif
