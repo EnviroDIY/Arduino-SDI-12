d01fb35081ea8592b10aa7fe122107b83ea1da44
Sara Damiano <sdamiano@stroudcenter.org>
2020-08-31 15:00:52 -0400

    Fix other slashes and directory

--- docs/fixXmlSections.py
+++ docs/fixXmlSections.py
@@ -7,25 +7,25 @@ import xml.etree.ElementTree as ET
 
 fileDir = os.path.dirname(os.path.realpath("__file__"))
 # print("Program Directory: {}".format(fileDir))
-relative_dir = "..\\..\\Arduino-SDI-12Doxygen\\xml\\"
+relative_dir = "../../Arduino-SDI-12Doxygen/xml/"
 abs_file_path = os.path.join(fileDir, relative_dir)
 abs_file_path = os.path.abspath(os.path.realpath(abs_file_path))
 # print("XML Directory: {}".format(fileDir))
 
 output_file = "examples.dox"
 read_mes = [
-    # '..\\Arduino-SDI-12Doxygen\\xml_8ino-example.xml',
-    "..\\..\\Arduino-SDI-12Doxygen\\xml\\a_wild_card_8ino-example.xml",
-    "..\\..\\Arduino-SDI-12Doxygen\\xml\\b_address_change_8ino-example.xml",
-    "..\\..\\Arduino-SDI-12Doxygen\\xml\\c_check_all_addresses_8ino-example.xml",
-    "..\\..\\Arduino-SDI-12Doxygen\\xml\\d_simple_logger_8ino-example.xml",
-    "..\\..\\Arduino-SDI-12Doxygen\\xml\\e_simple_parsing_8ino-example.xml",
-    "..\\..\\Arduino-SDI-12Doxygen\\xml\\f_basic_data_request_8ino-example.xml",
-    "..\\..\\Arduino-SDI-12Doxygen\\xml\\g_terminal_window_8ino-example.xml",
-    "..\\..\\Arduino-SDI-12Doxygen\\xml\\h_SDI-12_slave_implementation_8ino-example.xml",
-    "..\\..\\Arduino-SDI-12Doxygen\\xml\\i_SDI-12_interface_8ino-example.xml",
-    "..\\..\\Arduino-SDI-12Doxygen\\xml\\j_external_pcint_library_8ino-example.xml",
-    "..\\..\\Arduino-SDI-12Doxygen\\xml\\k_concurrent_logger_8ino-example.xml",
+    # '../Arduino-SDI-12Doxygen/xml_8ino-example.xml',
+    "../../Arduino-SDI-12Doxygen/xml/a_wild_card_8ino-example.xml",
+    "../../Arduino-SDI-12Doxygen/xml/b_address_change_8ino-example.xml",
+    "../../Arduino-SDI-12Doxygen/xml/c_check_all_addresses_8ino-example.xml",
+    "../../Arduino-SDI-12Doxygen/xml/d_simple_logger_8ino-example.xml",
+    "../../Arduino-SDI-12Doxygen/xml/e_simple_parsing_8ino-example.xml",
+    "../../Arduino-SDI-12Doxygen/xml/f_basic_data_request_8ino-example.xml",
+    "../../Arduino-SDI-12Doxygen/xml/g_terminal_window_8ino-example.xml",
+    "../../Arduino-SDI-12Doxygen/xml/h_SDI-12_slave_implementation_8ino-example.xml",
+    "../../Arduino-SDI-12Doxygen/xml/i_SDI-12_interface_8ino-example.xml",
+    "../../Arduino-SDI-12Doxygen/xml/j_external_pcint_library_8ino-example.xml",
+    "../../Arduino-SDI-12Doxygen/xml/k_concurrent_logger_8ino-example.xml",
 ]
 all_files = [f for f in os.listdir(abs_file_path) if os.path.isfile(os.path.join(abs_file_path, f))]
 
