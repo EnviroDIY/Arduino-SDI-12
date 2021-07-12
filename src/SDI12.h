19449f106fb046b491f78e27a39b65f35258dc1a
DalesLandNet <peterj@daleslandnet.uk>
2021-07-12 16:08:32 +0100

    Correct missing #, Also add CRC calculate function and correct one of the overloaded methods

--- src/SDI12.h
+++ src/SDI12.h
@@ -909,6 +909,12 @@ class SDI12 : public Stream {
   void sendCommand(const char* cmd, int8_t extraWakeTime = SDI12_WAKE_DELAY);
   /// @copydoc SDI12::sendCommand(String&, int8_t)
   void sendCommand(FlashString cmd, int8_t extraWakeTime = SDI12_WAKE_DELAY);
+ #ifdef USE_CRC
+  String addCRCResponse(String &resp);     // Add CRC to the  resp  string (for slave use)
+  char * addCRCResponse( char *resp);      // Add CRC to the  resp  string (for slave use)
+  String addCRCResponse(FlashString resp); // Add CRC to the  resp  string (for slave use)
+  String calculateCRC(String &resp);       // Calculate the CRC for a response
+ #endif
 
   /**
    * @brief Send a response out on the data line (for slave use)
