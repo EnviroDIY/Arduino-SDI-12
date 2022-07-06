b20ef6ae49bf244da6496d9d46093010d393a625
Sara Damiano <sdamiano@stroudcenter.org>
2022-07-06 14:09:51 -0400

    Add empty file
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

---
+++ src/SDI12.h
@@ -0,0 +1,54 @@
+/**
+ * @file SDI12.h
+ * @copyright (c) 2013-2020 Stroud Water Research Center (SWRC)
+ *                          and the EnviroDIY Development Team
+ * @date August 2013
+ * @author Kevin M.Smith <SDI12@ethosengineering.org>
+ *
+ * @brief This is an empty file for the Arduino IDE on Linux
+ *
+ * ========================== Arduino SDI-12 ==================================
+ *
+ * An Arduino library for SDI-12 communication with a wide variety of environmental
+ * sensors. This library provides a general software solution, without requiring any
+ * additional hardware.
+ *
+ * ======================== Attribution & License =============================
+ *
+ * Copyright (C) 2013  Stroud Water Research Center
+ * Available at https://github.com/EnviroDIY/Arduino-SDI-12
+ *
+ * Authored initially in August 2013 by:
+ *          Kevin M. Smith (http://ethosengineering.org)
+ *          Inquiries: SDI12@ethosengineering.org
+ *
+ * Modified 2017 by Manuel Jimenez Buendia to work with ARM based processors (Arduino
+ * Zero)
+ *
+ * Maintenance and merging 2017 by Sara Damiano
+ *
+ * based on the SoftwareSerial library (formerly NewSoftSerial), authored by:
+ *         ladyada (http://ladyada.net)
+ *         Mikal Hart (http://www.arduiniana.org)
+ *         Paul Stoffregen (http://www.pjrc.com)
+ *         Garrett Mace (http://www.macetech.com)
+ *         Brett Hagman (http://www.roguerobotics.com/)
+ *
+ * This library is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation; either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this library; if not, write to the Free Software Foundation, Inc., 51 Franklin
+ * Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+#ifndef SRC_SDI12_H_
+#define SRC_SDI12_H_
+
+#endif  // SRC_SDI12_H_
